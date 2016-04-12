220e57343a4901e81eb9d23fac5d34ebd4ec8ea4
raba930 <rabuzin.antonio@gmail.com>
2016-04-12 23:24:18 +0200

    04/08 form

---
+++ 4-form.js
@@ -0,0 +1,9 @@
+var express = require('express'),
+	app = express(),
+	bodyparser = require('body-parser');
+
+app.use(bodyparser.urlencoded({extended: false}));
+app.post('/form', function(req, res) {
+  res.end(req.body.str.split('').reverse().join(''));
+})
+app.listen(process.argv[2]);
